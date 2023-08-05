1da1e17d9659c2a4d497d4a1a8df3bc6121960f9
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-08-06 01:47:30 +0500

    driver controller has been added

---
+++ Controllers/DriverController.cs
@@ -0,0 +1,22 @@
+﻿using DemoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DriverController : Controller
+    {
+        // let's make an in-memory database
+        private readonly List<Driver> _drivers = new List<Driver>() { 
+            new Driver {Id=0, Name="John", DriverNumber=131, Team="Red Team"},
+            new Driver {Id=1, Name="James", DriverNumber=132, Team="Blue Team"},
+            new Driver {Id=2, Name="Murdock", DriverNumber=133, Team="Dev Team"}
+        };
+
+        public IActionResult Get()
+        {
+            return Ok(_drivers);
+        }
+    }
+}
---
+++ Models/Driver.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoAPI.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DriverNumber { get; set; }
+        public string Team { get; set; }
+    }
+}
