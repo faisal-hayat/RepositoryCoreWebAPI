3d8ab284229d172b5e8c75c3897f4cca78002f38
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-08-06 06:26:17 +0500

    repository pattern has been added

---
+++ Services/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using DemoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        public Task<IActionResult> Get();
+        public Task<IActionResult> Get(int id);
+        public Task<IActionResult> AddDriver(Driver driver);
+        public Task<IActionResult> DeleteDriver(int Id);
+        public Task<IActionResult> UpdateDriver(Driver driver);
+    }
+}
---
+++ Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using DemoAPI.Models;
+using DemoAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Services
+{
+    public class OrderService : IOrderService
+    {
+        [HttpGet]
+        public Task<IActionResult> AddDriver(Driver driver)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteDriver(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> UpdateDriver(Driver driver)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
